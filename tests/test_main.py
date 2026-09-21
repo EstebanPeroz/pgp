405e82e0816cda0f43b8c02f5c688c22675855e0
Noah Savoye <noah.savoye@epitech.eu>
2026-09-21 23:10:04 +0200

    feat(test): add input parser tests for valid arguments

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,3 +4,18 @@
 # def test_main_prints_hello(capsys):
 #     main()
 #     assert capsys.readouterr().out == "Hello World!\n"
+
+import sys
+from unittest.mock import patch
+
+from src.my_pgp.input_parser import input_parser
+
+
+def test_input_parser_valid_args():
+    test_args = ["my_pgp", "xor", "-c", "mykey"]
+    with patch.object(sys, "argv", test_args):
+        args = input_parser()
+        assert args.crypto_system == "xor"
+        assert args.c is True
+        assert args.d is False
+        assert args.key == "mykey"
