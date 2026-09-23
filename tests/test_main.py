0cb74553462559ce577b4838ab4774469c3d307d
Esteban Peroz <esteban@peroz.fr>
2026-09-23 17:57:50 +0200

    test(parsing): Fixed parsing test

--- tests/test_main.py
+++ tests/test_main.py
@@ -10,12 +10,14 @@ from unittest.mock import patch
 
 from src.my_pgp.input_parser import input_parser
 
+key: str = "576861742069732064656164206d6179206e6576657220646965"
+
 
 def test_input_parser_valid_args():
-    test_args = ["my_pgp", "xor", "-c", "mykey"]
+    test_args = ["my_pgp", "xor", "-c", key]
     with patch.object(sys, "argv", test_args):
         args = input_parser()
         assert args.crypto_system == "xor"
         assert args.c is True
         assert args.d is False
-        assert args.key == "mykey"
+        assert args.key.hex() == key
